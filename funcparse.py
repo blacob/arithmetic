114026b5d8a6f06c7e1eaef031f73efbfc6fcfa5
Jacob Bland <jrbland@berkeley.edu>
2020-02-26 10:51:59 -0800

    implemented more tests. cleaned up

--- funcparse.py
+++ funcparse.py
@@ -3,27 +3,26 @@ digits = '0123456789.'
 operators = '+-*'
 parens = '()'
 
+"""
+Parses a list of tokens EXPLST, returning a tuple where the first element is
+it's evaluation.
+"""
 def parse(explst):
-
     first = explst[0]
     rest = explst[1:]
     if first == '(':
-        left, newlist = parse(explst[1:])
-        #print(left, newlist)
+        left, newlist = parse(rest) 
         operator = newlist[0]
         right, toss = parse(newlist[1:])
-        #print(right, toss)
         return eval(left, operator, right), toss[1:]
-    elif first[0] in digits or first in operators:
-        return first, explst[1:]
+    return first, rest
 
-
-
+"""
+Takes numerical types L and R and applies function that OPERATOR represents
+"""
 def eval(l, operator, r):
-    left = int(l)
-    if not r:
-        return left
-    right = int(r)
+    left = float(l)
+    right = float(r)
     if operator == '+':
         return left + right
     elif operator == '-':
@@ -34,7 +33,9 @@ def eval(l, operator, r):
         print("ERROR, invalid op: " + operator)
         exit()
 
-""" Receives a parenthesized arithmetic expression and returns it's value. """
+"""
+Receives a parenthesized arithmetic expression LINE and returns it's evaluation.
+"""
 def driver(line):
     holder = []
     cleaned = line.replace(' ', '')
@@ -42,24 +43,32 @@ def driver(line):
         if c in parens or c in operators:
             holder.append(c)
         elif c in digits:
-            if len(holder) == 0 or not (holder[-1] in digits):
+            if len(holder) == 0 or not (holder[-1][0] in digits):
                 holder.append(c)
             else:
                 holder[-1] = holder[-1] + c
         else:
             return "Invalid Input: " + line
-    return parse(holder)[0]
+    try:
+        return parse(holder)[0]
+    except IndexError:
+        return "Invalid Input: " + line
+
 
 def main():
      for i in range(1, len(sys.argv)):
          print(driver(sys.argv[i]))
-     print("\n Example Tests: ")
-     print("Input: '(1+3)' ... Expected: 4 ... Got: " + str(driver("(1+3)")))
+     print("\nExample Tests: ")
+     print("Input: '(521+3)' ... Expected: 524 ... Got: " + str(driver("(521+3)")))
      print("Input: '(4-2)' ...  Expected: 2 ... Got: " + str(driver("(4-2)")))
-     print("Input: '4-3' ...  Expected: 1 ... Got: " + str(driver("4-3")))
      print("Input: '((4-2)+(3*2))' ...  Expected: 8 ... Got: " + str(driver("((4-2)+(3*2))")))
      print("Input: '((1+(3*2))-4)' ... Expected: 3 ... Got: " + str(driver("((1+(3*2))-4)")))
      print("Input: '3' ... Expected: 3 ... Got: " + str(driver("3")))
+     print("Input: '(5.6+2.4)' ... Expected: 8 ... Got: " + str(driver("(5.6+2.4)")))
+     print(str(driver("(6)")))
+     print(str(driver("4-3")))
+
+
      print(driver('bill'))
      print(driver('(bill-bill)'))
 
