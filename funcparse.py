188f9ed8fc4e748e54f73d2d9ba2df0639adfacf
Jacob Bland <jrbland@berkeley.edu>
2020-02-28 10:05:12 -0800

    achieved functionality, refactored, and commented

--- funcparse.py
+++ funcparse.py
@@ -3,59 +3,59 @@ import operator
 import sys
 from functools import reduce
 digits = '0123456789.'
-operators = '+-*'
-parens = '()'
+operators = '+-*()'
+
+
 
 """
 Parses a list of tokens EXPLST, returning a tuple where the first element is
 it's evaluation.
+Acknowledgement: This is largely based upon Andrey Vlasovskikh's implementation
+    of an expression calculator, as seen in the funcparserlib tutorial.
 """
 def parse(tokens):
-    print(tokens)
     const = lambda x: lambda _: x
     unarg = lambda f: lambda x: f(*x)
-
     makeop = lambda s, f: op(s) >> const(f)
-
-    def make_number(s):
+    #converting string representing number to respective numeric type
+    def makenum(s):
         try:
             return int(s)
         except ValueError:
             return float(s)
 
-    def eval_expr(z, lst):
-        return reduce((lambda s, a: a[0](s, a[1])), lst, z)
-    eval = unarg(eval_expr)
+    #evaluator parser 
+    eval = unarg(lambda z, lst: reduce((lambda s, a: a[0](s, a[1])), lst, z))
 
-    number = some(lambda tok: tok[0] in digits)
+    #define parsers for primitive types (nums and arithmetic ops)
+    number = some(lambda tok: tok[0] in digits) >> makenum
     op = lambda s: some(lambda tok: (tok in operators) and (tok == s))
-    op_ = lambda s: skip(op(s))
+    skipop = lambda s: skip(op(s))
 
+    #setup basic operations
     add = makeop('+', operator.add)
     sub = makeop('-', operator.sub)
     mul = makeop('*', operator.mul)
-
     add_op = add | sub
-
     @with_forward_decls
-    def primary():
-        return number | (op_('(') + expr + op_(')'))
+    def parens():
+        return number | (skipop('(') + expr + skipop(')'))
 
-    term = primary + many(mul + primary) >> eval
+    #composing series of parsers
+    term = parens + many(mul + parens) >> eval
     expr = term + many(add_op + term) >> eval
+    toplevel = maybe(expr) + skip(finished)
 
-
-    toplevel = maybe(expr) #+ finished
     return toplevel.parse(tokens)
 
 """
 Receives a parenthesized arithmetic expression LINE and returns it's evaluation.
 """
-def driver(line):
+def drive(line):
     holder = []
     cleaned = line.replace(' ', '')
     for c in cleaned:
-        if c in parens or c in operators:
+        if c in operators:
             holder.append(c)
         elif c in digits:
             if len(holder) == 0 or not (holder[-1][0] in digits):
@@ -64,26 +64,23 @@ def driver(line):
                 holder[-1] = holder[-1] + c
         else:
             return "Invalid Input: " + line
-
-    try:
-        return parse(holder)
-    except IndexError:
-        return "Invalid Input: " + line
-
+    return parse(holder)
 
 def main():
      for i in range(1, len(sys.argv)):
          print(driver(sys.argv[i]))
+
+     form = "Input: {} ... Expected: {} ... Actual: {}"
      print("\nExample Tests: ")
-     print("Input: '(2+3)' ... Expected: 5 ... Got: " + str(driver("(2+3)")))
-     print("Input: '(4-2)' ...  Expected: 2 ... Got: " + str(driver("(4-2)")))
-     print("Input: '((4-2)+(3*2))' ...  Expected: 8 ... Got: " + str(driver("((4-2)+(3*2))")))
-     print("Input: '((1+(3*2))-4)' ... Expected: 3 ... Got: " + str(driver("((1+(3*2))-4)")))
-     print("Input: '3' ... Expected: 3 ... Got: " + str(driver("3")))
-     print("Input: '(5.6+2.4)' ... Expected: 8 ... Got: " + str(driver("(5.6+2.4)")))
-     print(str(driver("(6)")))
-     print(driver('bill'))
-     print(driver('(bill-bill)'))
+     print(form.format("(52*2)", 104, drive("(52*2)")))
+     print(form.format("4-2", 2, drive("4-2")))
+     print(form.format("((4-2)+(3*2))", 8, drive("((4-2)+(3*2))")))
+     print(form.format("((1+(3*2))-4)", 3, drive("((1+(3*2))-4)")))
+     print(form.format("(5.6+2.4)", 8, drive("(5.6+2.4)")))
+     print(form.format("(6)", 6, drive("(6)")))
+
+     print(drive('bill'))
+     print(drive('(bill-bill)'))
 
 if __name__== "__main__":
     main()
